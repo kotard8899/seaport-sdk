804f43a5e3db35b1900546d1fe13ab358edac78b
ryan <sally41176789@yahoo.com.tw>
2022-10-18 17:04:47 +0800

    fix: add factories

---
+++ factories/index.js
@@ -0,0 +1,5 @@
+
+export { ERC1155__factory } from './ERC1155__factory';
+export { ERC20__factory } from './ERC20__factory';
+export { ERC721__factory } from './ERC721__factory';
+export { Seaport__factory } from "./Seaport__factory"
